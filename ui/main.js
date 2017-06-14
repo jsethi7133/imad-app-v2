36729315f256d01d02f73a1c1e18946da594b0c2
jsethi7133 <jsethi7133@gmail.com>
2017-06-14 11:32:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,8 @@ element.innerHTML='New value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+'px';
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
    var interval=setInterval(moveRight,100);
