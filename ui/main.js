f3ac6e9e56cb71649d1b9f40c905647bc6a5c8bf
jsethi7133 <jsethi7133@gmail.com>
2017-06-16 11:02:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 //change the text of the main-text div
 var element=document.getElementById('main-text');
@@ -14,4 +14,20 @@ function moveRight(){
 }
 img.onclick=function(){
    var interval=setInterval(moveRight,100);
+}; */
+
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
+
+button.onclick=function()
+{
+    //Make a request to cunter end point
+    
+    //Capture the response and store it as a variable
+    
+    //Render the variable in the current span
+    counter =counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
