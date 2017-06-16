59da52e9e73e79ec4d7c3f0197f2a4c97e2dc34a
jsethi7133 <jsethi7133@gmail.com>
2017-06-16 10:49:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/', function (req, res) {
 
 var counter=0;
 app.get('/counter',function(req,res){
-    counter=couter+1;
+    counter=counter+1;
     res.send(counter.toString());
 });
 app.get('/:articleName',function(req,res)
